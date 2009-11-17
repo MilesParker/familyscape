36af27a7814e3225fa5c708c5daddcccec655f05
Miles Parker <milesparker@gmail.com>
2009-11-17 00:35:56 +0000

    Update to latest 3D libraries.

--- src/edu/brook/ccf/rpm/RPP3D.java
+++ src/edu/brook/ccf/rpm/RPP3D.java
@@ -1,7 +1,8 @@
 package edu.brook.ccf.rpm;
 
-import org.ascape.view.vis.GEFView;
+import org.eclipse.amp.agf3d.AgentEdit3DPart;
 import org.eclipse.amp.agf3d.I3DModelFactoryProvider;
+import org.eclipse.amp.agf3d.RenderingShapeProvider;
 import org.eclipse.amp.escape.ascape.adapt.Ascape3DPartFactory;
 import org.eclipse.amp.escape.ascape.agf3d.GEF3DView;
 import org.eclipse.gef.EditPart;
@@ -9,7 +10,6 @@ import org.eclipse.gef.EditPartFactory;
 
 public class RPP3D extends RelationshipsAndPregnancy implements I3DModelFactoryProvider {
     public void createGraphicViews() {
-        addView(new GEFView());
         addView(new GEF3DView());
     }
 
@@ -17,7 +17,7 @@ public class RPP3D extends RelationshipsAndPregnancy implements I3DModelFactoryP
         return new Ascape3DPartFactory() {
             public EditPart getPartForElement(Object model) {
                 if (model instanceof Adult) {
-                    return new RenderGender3DEditPart();
+                    return new AgentEdit3DPart(RenderingShapeProvider.createProvider(new IndividualRenderer()), null);
                 }
                 // if (model instanceof NeighborhoodCell) {
                 // return new Tile3DEditPart();
