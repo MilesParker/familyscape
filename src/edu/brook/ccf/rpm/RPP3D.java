cf0e50803a85ad34b3fd279bfbbc88f4d20082a3
Miles Parker <milesparker@gmail.com>
2010-10-11 21:35:12 +0000

    Update changes.

--- src/edu/brook/ccf/rpm/RPP3D.java
+++ src/edu/brook/ccf/rpm/RPP3D.java
@@ -1,54 +1,35 @@
 package edu.brook.ccf.rpm;
 
-import org.ascape.util.vis.ColorFeature;
-import org.eclipse.amp.agf3d.AgentEdit3DPart;
-import org.eclipse.amp.agf3d.I3DModelFactoryProvider;
-import org.eclipse.amp.agf3d.RenderingShapeProvider;
-import org.eclipse.amp.escape.ascape.adapt.Ascape3DPartFactory;
+import org.ascape.view.vis.ChartView;
+import org.eclipse.amp.agf3d.IGraphics3DAdapted;
+import org.eclipse.amp.agf3d.IGraphics3DAdapter;
+import org.eclipse.amp.agf3d.IShape3DProvider;
+import org.eclipse.amp.agf3d.Shapes3D;
+import org.eclipse.amp.escape.ascape.adapt.AscapeDefaultGraphics3DAdapter;
 import org.eclipse.amp.escape.ascape.agf3d.GEF3DView;
-import org.eclipse.gef.EditPart;
-import org.eclipse.gef.EditPartFactory;
-import org.eclipse.jface.viewers.IColorProvider;
-import org.eclipse.swt.graphics.Color;
 
-public class RPP3D extends RelationshipsAndPregnancy implements I3DModelFactoryProvider {
+public class RPP3D extends RelationshipsAndPregnancy implements IGraphics3DAdapted {
     public void createGraphicViews() {
         addView(new GEF3DView());
+        addView(new ChartView());
     }
 
-    public EditPartFactory getEditPart3DFactory() {
-        return new Ascape3DPartFactory() {
-            public EditPart getPartForElement(Object model) {
-                if (model instanceof Adult) {
-                    return new AgentEdit3DPart(RenderingShapeProvider.createProvider(new IndividualRenderer()), null);
-                }
-                // if (model instanceof NeighborhoodCell) {
-                // return new Tile3DEditPart();
-                // }
-                return super.getPartForElement(model);
+    private static final IShape3DProvider[] HUMAN_SHAPE_PROVIDERS = new IShape3DProvider[] { Shapes3D.HUMAN_SHAPE_PROVIDER};
+
+    IGraphics3DAdapter adapter = new AscapeDefaultGraphics3DAdapter() {
+        public IShape3DProvider[] getShapes(Object object) {
+            if (object instanceof Adult) {
+                return HUMAN_SHAPE_PROVIDERS;
             }
-            IColorProvider color = new IColorProvider() {
-
-                public Color getForeground(Object arg0) {
-                    return ColorFeature.WHITE;
-                }
-
-                public Color getBackground(Object arg0) {
-                    return ColorFeature.LIGHT_GRAY;
-                }
-            };
-
-            /**
-             * @param object
-             * @return
-             * @see org.eclipse.amp.escape.ascape.adapt.Ascape3DPartFactory#getColorProvider(java.lang.Object)
-             */
-            public IColorProvider getColorProvider(Object object) {
-                if (object instanceof NeighborhoodCell) {
-                    return color;
-                }
-                return super.getColorProvider(object);
-            }
+            return super.getShapes(object);
         };
+    };
+
+    /**
+     * @return
+     * @see org.eclipse.amp.agf3d.IGraphics3DAdapted#getGraphics3DAdapter()
+     */
+    public IGraphics3DAdapter getGraphics3DAdapter() {
+        return adapter;
     }
 }
